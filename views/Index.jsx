8da9a93269539707b8c2f225f9b29500a0dd49bd
Mahamadou <mmohmo@yahoo.com>
2023-09-10 22:55:24 -0400

    index.jsx styled

--- views/Index.jsx
+++ views/Index.jsx
@@ -6,7 +6,12 @@ const Index = (props) => {
     return(
         <DefaultLayout title="Captains Log">
             
-            <a href="/logs/new">Create a New Log</a>
+            <a 
+                className="mt-8 font-medium text-lg text-blue-700 hover:text-blue-400"
+                href="/logs/new"
+            >
+                Create a New Log
+            </a>
             <ul className="divide-y divide-gray-100">
                 {
                     logs.map(log => {
@@ -15,13 +20,22 @@ const Index = (props) => {
                             <li className="flex justify-between gap-x-6 py-5" key={log._id}>
                                 
                                 <form method="POST" action={`/api/logs/${log._id}?_method=DELETE`}>
-                                    <input type="submit" value="Delete Log"/>
+                                    <input 
+                                        type="submit" 
+                                        value="Delete Log"
+                                        className="rounded-md bg-blue-900 px-2.5 py-1.5 text-sm font-semibold text-slate-100 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-blue-700"
+                                    />
                                 </form>
                                 <div className="min-w-0 flex-auto">
                                     <a  className="text-md font-semibold leading-6 text-gray-900" href={`/logs/${log._id}`}>{log.title}</a>
                                 </div>
                                 
-                                <a href={`/logs/${log._id}/edit`}>Edit log</a>
+                                <a 
+                                    href={`/logs/${log._id}/edit`}
+                                    className="rounded-md bg-blue-900 px-2.5 py-1.5 text-sm font-semibold text-slate-100 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-blue-700"
+                                >
+                                    Edit log
+                                </a>
                             </li>
                         )
                     })
